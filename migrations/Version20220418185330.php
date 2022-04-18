ae62563808b9c5ddc29fdebeeb75cdd79296a785
michalne <michalsmola0502@gmail.com>
2022-04-18 21:39:18 +0200

    create migration

---
+++ migrations/Version20220418185330.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220418185330 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE weather (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, country VARCHAR(255) NOT NULL, temperature DOUBLE PRECISION NOT NULL, feels_like_temperature DOUBLE PRECISION NOT NULL, weather_description VARCHAR(255) NOT NULL, weather_icon VARCHAR(255) NOT NULL, wind_direction VARCHAR(255) NOT NULL, pressure DOUBLE PRECISION NOT NULL, uv_index DOUBLE PRECISION NOT NULL, data_time DATE NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE weather');
+    }
+}
