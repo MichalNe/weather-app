2a106d61d17c4067539fbde409fa23670cbd59d8
michalne <michalsmola0502@gmail.com>
2022-04-29 22:23:15 +0200

    add query parameter

--- src/Domains/Weather/DomainModel/WeatherRepository.php
+++ src/Domains/Weather/DomainModel/WeatherRepository.php
@@ -2,11 +2,12 @@
 
 namespace App\Domains\Weather\DomainModel;
 
+use App\Domains\Weather\Application\Query\GetWeatherData;
 use App\Domains\Weather\ReadModel\WeatherDTO;
 
 interface WeatherRepository
 {
-    public function getWeatherData(): WeatherDTO;
+    public function getWeatherData(GetWeatherData $query): WeatherDTO;
 
     public function setWeatherData(WeatherInformation $weatherInformation): void;
 }
