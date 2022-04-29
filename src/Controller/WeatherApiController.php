daabb68fcaedead90c319ffe2ef7012361bd14ec
michalne <michalsmola0502@gmail.com>
2022-04-29 22:22:18 +0200

    use readModel to get weather data

--- src/Controller/WeatherApiController.php
+++ src/Controller/WeatherApiController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Domains\Weather\Application\Command\SetWeatherData;
+use App\Domains\Weather\Application\Query\GetWeatherData;
 use App\Domains\Weather\Application\WeatherApiService;
+use App\Domains\Weather\ReadModel\WeatherReadModel;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,11 +16,16 @@ class WeatherApiController extends AbstractController
 
     public function __construct(
         private WeatherApiService $weatherApiService,
+        private WeatherReadModel $weatherReadModel
     ) {}
 
     #[Route('/api/weather/current/{parameter}', name: 'app_weather_api', methods: ['GET'])]
     public function index(string $parameter = self::DEFAULT_PARAMETER): Response
     {
+        $query = new GetWeatherData($parameter);
+
+        $this->weatherReadModel->getWeatherData($query);
+
         return new Response('');
     }
 
