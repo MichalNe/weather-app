f93ca7b9e69c30027ffe83e740fb773d12f73a5d
Nerwo <michalsmoladev@gmail.com>
2022-06-11 15:24:41 +0200

    create getWeatherData method

--- src/Domains/Weather/Infrastructure/WeatherDbRepository.php
+++ src/Domains/Weather/Infrastructure/WeatherDbRepository.php
@@ -3,33 +3,35 @@ declare(strict_types=1);
 
 namespace App\Domains\Weather\Infrastructure;
 
+use App\Domains\Weather\DomainModel\WeatherDTOFactory;
 use App\Domains\Weather\DomainModel\WeatherEntityFactory;
 use App\Domains\Weather\DomainModel\WeatherInformation;
 use App\Domains\Weather\DomainModel\WeatherRepository;
+use App\Domains\Weather\ReadModel\Query\GetWeatherData;
 use App\Domains\Weather\ReadModel\WeatherDTO;
 use App\Entity\Weather;
-use Doctrine\DBAL\Query\QueryBuilder;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Response;
 
 class WeatherDbRepository implements WeatherRepository
 {
-
+    private const LIMIT = 1;
+    private const ORDER_BY_KEY = 'DESC';
 
     public function __construct(
         private EntityManagerInterface $entityManager
     ) {}
 
-    public function getWeatherData(): WeatherDTO
+    public function getWeatherData(GetWeatherData $query): WeatherDTO
     {
-        $query = $this->entityManager->createQuery(
-            'SELECT *
-             FROM weather'
+        $sql = $this->entityManager->getRepository(Weather::class);
+
+        $result = $sql->findBy(
+            ['name' => $query->getParameter()],
+            ['id' => self::ORDER_BY_KEY],
+            self::LIMIT
         );
 
-        $result = $query->getResult();
-
-        dd($result);
+        return WeatherDTOFactory::create($result[0]->toArray());
     }
 
     public function setWeatherData(WeatherInformation $weatherInformation): void
