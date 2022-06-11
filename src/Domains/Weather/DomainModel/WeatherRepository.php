de1f1cde5086980082d3cdfa9f5b11673b07c676
Nerwo <michalsmoladev@gmail.com>
2022-06-11 15:24:02 +0200

    use query in parameter

--- src/Domains/Weather/DomainModel/WeatherRepository.php
+++ src/Domains/Weather/DomainModel/WeatherRepository.php
@@ -2,12 +2,11 @@
 
 namespace App\Domains\Weather\DomainModel;
 
-use App\Domains\Weather\Application\Query\GetWeatherData;
+use App\Domains\Weather\ReadModel\Query\GetWeatherData;
 use App\Domains\Weather\ReadModel\WeatherDTO;
 
 interface WeatherRepository
 {
     public function getWeatherData(GetWeatherData $query): WeatherDTO;
-
     public function setWeatherData(WeatherInformation $weatherInformation): void;
 }
